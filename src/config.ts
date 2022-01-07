50a376e05498291cbed1fc8ac8bccb4a7b199264
alessandro <alessandroconectado@gmail.com>
2022-01-07 06:39:25 +0044

    feat: :sparkles: add url of production and development api

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,6 @@
-export const URL_API = 'http://192.168.100.2:3003/api/v1/';
+const ENV = 'dev';
+
+export const URL_API =
+  ENV === 'dev'
+    ? 'http://192.168.100.2:3003/api/v1/'
+    : 'http://tp-load-balancer-1809039677.us-east-1.elb.amazonaws.com';
--- src/services/api.ts
+++ src/services/api.ts
@@ -13,17 +13,13 @@ const instance = axios.create({
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
   },
   async function (error) {
-    if (error.response.status === 401) {
+    if (error.response?.status === 401) {
       return await AsyncStorage.multiRemove(['@token', '@user', '@pushToken']);
     }
 
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     return Promise.reject(error);
   },
 );
